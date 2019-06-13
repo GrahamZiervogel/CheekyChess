92545cb9979117df6eb90d2698127a9494d41576
Graham Ziervogel <grahamziervogel@gmail.com>
2019-06-13 20:59:59 +0200

    Add copyright notice to project files.
    Using a template in the style of Microsoft All Times Lucky's copyright is asserted.

--- app/src/main/java/com/alltimeslucky/cheekychess/model/piece/Queen.kt
+++ app/src/main/java/com/alltimeslucky/cheekychess/model/piece/Queen.kt
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) All Times Lucky. All rights reserved.
+ * Licensed under the GNU Lesser General Public License v3.0. See LICENCE file in the project root for full licence information.
+ *
+ */
+
 package com.alltimeslucky.cheekychess.model.piece
 
 class Queen : Piece() {
